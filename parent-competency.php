87eb61482ed082e68a3741751079b066d82b6d7b
WildanZulkarnaen <wildan.zulkarnaen@kumparan.com>
2017-08-15 17:52:17 +0700

    update experience and loader

---
+++ parent-competency.php
@@ -0,0 +1,130 @@
+<?php
+$title ="Competency";
+include 'templates/header.php';
+ ?>
+ <div class="banner-hero">
+   <div class="banner-hero-item" style="background-image:url('images/bg-competency.jpg')">
+     <div class="overlay-base"></div>
+      <div class="caption-hero">
+        <div class="container">
+          <h1>Reliability Competency</h1>
+        </div>
+      </div>
+   </div>
+ </div>
+ <div class="container">
+   <div class="container-detail container-overlap">
+     <div class="text-center">
+       <p class="article-text">
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+       </p>
+     </div>
+   </div>
+   <div class="row">
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Operating Deflection Shape (ODS) for Machinery Troubleshooting</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Card Title</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Reliability Technology for Turbine</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Card Title</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Card Title</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Reliability Technology for Turbine</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Card Title</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+     <div class="col s12 l3">
+       <a href="#" class="card-linkable">
+         <div class="card small">
+           <div class="card-image">
+             <img class="responsive-img" src="images/sample-asset-card.jpg">
+           </div>
+           <div class="card-content">
+             <h3 class="card-title">Card Title</h3>
+           </div>
+         </div>
+       </a>
+     </div>
+
+   </div>
+   <div class="text-center margbot">
+     <a href="#" class="waves-effect waves-light btn btn-large btn-flat btn-outline-primary">Load More</a>
+   </div>
+ </div>
+
+
+ <?php
+include 'templates/footer.php';
+  ?>
